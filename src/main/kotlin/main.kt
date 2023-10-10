43f2ed164f8444b7bfbb7b6a1801b4d53d2b435b
Yaroslav Borisov <yarx@ya.ru>
2023-10-10 16:35:03 +0300

    Initial commit

---
+++ src/main/kotlin/main.kt
@@ -0,0 +1,12 @@
+fun main() {
+    val isRegularCustomer = true
+    val amount = 2500
+
+    var discount = if (amount <= 1_000) 0 else if (amount<=10_000) 100 else (amount*0.05).toInt()
+    discount += if (isRegularCustomer) (amount * 0.01).toInt() else 0
+
+    println("Стоимость покупок с учетом скидки составит ${amount - discount} руб")
+
+
+
+}
