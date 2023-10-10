a1637bb01584c08a41066b567923368bfa653d66
Yaroslav Borisov <yarx@ya.ru>
2023-10-10 18:13:38 +0300

    Patch

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -3,10 +3,8 @@ fun main() {
     val amount = 2500
 
     var discount = if (amount <= 1_000) 0 else if (amount<=10_000) 100 else (amount*0.05).toInt()
-    discount += if (isRegularCustomer) (amount * 0.01).toInt() else 0
+    discount += if (isRegularCustomer) ((amount - discount) * 0.01).toInt() else 0
 
     println("Стоимость покупок с учетом скидки составит ${amount - discount} руб")
-
-
 
 }
